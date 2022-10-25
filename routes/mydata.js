ed236b138321e8958c91ede730dc30bf9da3bb86
BharathMopuru <95495923+BharathMopuru@users.noreply.github.com>
2022-10-24 22:51:48 -0500

    mydata.js and mydata.pug is modified

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BHARATH KUMAR REDDY MOPURU' });
+  res.render('mydata', { title: 'BHARATH KUMAR REDDY MOPURU' });
 });
 
 module.exports = router;
